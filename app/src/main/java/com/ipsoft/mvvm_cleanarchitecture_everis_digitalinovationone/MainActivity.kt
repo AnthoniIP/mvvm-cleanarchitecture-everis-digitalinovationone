66d4a31e9494032be3fc055f142df3bc0f695ec7
AnthoniIP <anthoni.ipiranga@gmail.com>
2021-02-18 18:02:04 -0300

    First Commit.

---
+++ app/src/main/java/com/ipsoft/mvvm_cleanarchitecture_everis_digitalinovationone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ipsoft.mvvm_cleanarchitecture_everis_digitalinovationone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
